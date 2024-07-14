49e81e2e8667c01d0a1e9a89fdc27176d92d7719
BayerTobias <tbayer2@gmx.de>
2024-07-14 14:35:53 +0200

    base-class model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BaseCharacter } from './home/models/base-character.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'character_planer';
+
+  caracterData = {
+    name: 'Milhaus Manasturm',
+    race: 'gnome',
+    class: 'mage',
+    maxHealth: 35,
+
+    strengthValue: 5,
+    agilityValue: 5,
+    constitutionValue: 6,
+    intelligenceValue: 9,
+    charismaValue: 6,
+  };
+
+  character = new BaseCharacter(this.caracterData);
+
+  createCharacter() {
+    console.log(this.character);
+  }
 }
---
+++ src/app/home/models/base-character.model.ts
@@ -0,0 +1,93 @@
+interface BaseCharacterData {
+  name: string;
+  race: string;
+  class: string;
+
+  maxHealth: number; // Calculate class based when final
+
+  strengthValue: number;
+  agilityValue: number;
+  constitutionValue: number;
+  intelligenceValue: number;
+  charismaValue: number;
+}
+
+export class BaseCharacter {
+  name: string;
+  race: string;
+  class: string;
+  maxHealth: number;
+  strengthValue: number;
+  strengthBonus: number;
+  agilityValue: number;
+  agilityBonus: number;
+  constitutionValue: number;
+  constitutionBonus: number;
+  intelligenceValue: number;
+  intelligenceBonus: number;
+  charismaValue: number;
+  charismaBonus: number;
+
+  constructor(data: BaseCharacterData) {
+    this.name = data?.name || '';
+    this.race = data?.race || '';
+    this.class = data?.class || '';
+    this.maxHealth = data.maxHealth || 0;
+    this.strengthValue = data.strengthValue || 0;
+    this.strengthBonus = this.getStatBonusValue(this.strengthValue);
+    this.agilityValue = data.agilityValue || 0;
+    this.agilityBonus = this.getStatBonusValue(this.agilityValue);
+    this.constitutionValue = data.constitutionValue || 0;
+    this.constitutionBonus = this.getStatBonusValue(this.constitutionValue);
+    this.intelligenceValue = data.intelligenceValue || 0;
+    this.intelligenceBonus = this.getStatBonusValue(this.intelligenceValue);
+    this.charismaValue = data.charismaValue || 0;
+    this.charismaBonus = this.getStatBonusValue(this.charismaValue);
+  }
+
+  getStatBonusValue(statValue: number) {
+    switch (statValue) {
+      case 0:
+        return -99;
+        break;
+      case 1:
+        return -3;
+        break;
+      case 2:
+        return -2;
+        break;
+      case 3:
+      case 4:
+        return -1;
+        break;
+      case 5:
+        return 0;
+        break;
+      case 6:
+      case 7:
+        return 1;
+        break;
+      case 8:
+      case 9:
+        return 2;
+        break;
+      case 10:
+      case 11:
+        return 3;
+        break;
+      case 12:
+      case 13:
+        return 4;
+        break;
+      case 14:
+        return 5;
+        break;
+      default:
+        return -99;
+    }
+  }
+
+  asJson() {
+    return {};
+  }
+}
