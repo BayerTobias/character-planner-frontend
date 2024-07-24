dceb6241bb56c7569a6c51f3ef1a105ef630a0e2
BayerTobias <tbayer2@gmx.de>
2024-07-24 12:23:39 +0200

    login component

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  public loginForm: FormGroup;
+
+  private fb = inject(FormBuilder);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      rememberMe: [false],
+    });
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get rememberMe() {
+    return this.loginForm.get('rememberMe');
+  }
+
+  async login() {
+    console.log(this.username?.value);
+    console.log(this.password?.value);
+    console.log(this.rememberMe?.value);
+  }
+}
