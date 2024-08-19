dd1febeb0168e37962e8149119810cdc9b3e39e3
BayerTobias <tbayer2@gmx.de>
2024-08-19 11:42:31 +0200

    some skills display html and css

--- src/app/shared/components/character-components/skills-display/skills-display.component.ts
+++ src/app/shared/components/character-components/skills-display/skills-display.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Skill } from '../../../../home/models/skill.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills-display',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills-display.component.html',
   styleUrl: './skills-display.component.scss',
 })
