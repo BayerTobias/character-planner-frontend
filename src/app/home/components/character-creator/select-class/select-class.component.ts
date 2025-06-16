a8c0df70007eca53d4fbcafcc0ad7538af9665a5
BayerTobias <tbayer2@gmx.de>
2025-06-16 13:52:32 +0200

    select class error msg

--- src/app/home/components/character-creator/select-class/select-class.component.ts
+++ src/app/home/components/character-creator/select-class/select-class.component.ts
@@ -2,22 +2,33 @@ import { Component, inject } from '@angular/core';
 import { GameDataService } from '../../../../shared/services/game-data.service';
 import { CharClassListItem } from '../../../models/char-class-list-item.mode';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-select-class',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, CommonModule],
   templateUrl: './select-class.component.html',
   styleUrl: './select-class.component.scss',
 })
 export class SelectClassComponent {
   public gameDataService = inject(GameDataService);
+  private router = inject(Router);
 
   public selectedClass: CharClassListItem | null = null;
+  public nextClicked: boolean = false;
 
   selectClass(charClass: CharClassListItem) {
     this.selectedClass = charClass;
-    console.log(this.selectedClass);
+  }
+
+  checkIfClassSelected() {
+    this.nextClicked = true;
+  }
+
+  navigateToPreviousPage() {
+    this.router.navigateByUrl('/select-character');
   }
 
   getGermanName(className: string) {
